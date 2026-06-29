e7df856bd5f667e95fd3a426f7069524d1ec61fa
KuldeepPandit75 <kuldeepk11139@gmail.com>
2026-06-30 03:32:58 +0530

    uncommented the project comp

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Hero from "../../components/Hero/Hero";
 import OurServices from "../../components/OurServices/OurServices";
-// import Projects from "../../components/Projects/Projects";
+import Projects from "../../components/Projects/Projects";
 import Testimonials from "../../components/Testimonials/Testimonials";
 import FaqSection from "../../components/Services/FaqSection";
 import CtaSection from "../../components/Services/CtaSection";
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Hero />
       <OurServices />
-      {/* <Projects /> */}
+      <Projects />
       <Testimonials />
       <FaqSection />
       <CtaSection />
